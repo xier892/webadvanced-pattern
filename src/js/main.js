6676c63bbd8f8e11ae6e5f395c083fdcfd7c1029
Ricky Xie <xier892@newschool.edu>
2019-09-10 12:58:20 -0400

    add pattern based on modulus

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,36 @@
 const getRandomInt = (min, max) => Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min))) + Math.ceil(min);
 
+const fragment = document.createDocumentFragment();
 const grid = document.createElement('ul');
 grid.className = 'grid';
 
 const squares = [];
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 104; i++) {
+  const lightness = i % 2 === 0 ? getRandomInt(20, 40) : getRandomInt(80, 100);
   const square = document.createElement('li');
   square.style.width = '10%';
   square.style.margin = '1%';
-  square.style.backgroundColor = `hsl(0, 0%, ${getRandomInt(30, 100)}%)`;
   square.style.backgroundRepeat = 'repeat';
-  switch (true) {
-    case i <= 20:
+  square.style.backgroundColor = `hsl(0, 0%, ${lightness}%)`;
+  switch (0) {
+    case i % 2:
       square.style.backgroundImage = 'url("dist/assets/image/bedge-grunge.png")';
       break;
-    case i > 20 && i <= 40:
+    case i % 3:
       square.style.backgroundImage = 'url("dist/assets/image/bright-squares.png")';
       break;
-    case i > 40 && i <= 60:
-      square.style.backgroundImage = 'url("dist/assets/image/arabesque.png")';
-      break;
-    case i > 60 && i <= 80:
+    case i % 5:
       square.style.backgroundImage = 'url("dist/assets/image/candyhole.png")';
       break;
     default:
       square.style.backgroundImage = 'url("dist/assets/image/always-grey.png")';
   }
-  grid.appendChild(square);
+  fragment.appendChild(square);
   squares.push(square);
 }
 
+grid.appendChild(fragment);
 document.body.appendChild(grid);
 
 const resizeSquares = () => {
