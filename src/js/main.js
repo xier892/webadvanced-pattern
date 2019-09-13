01ffd9d057a818dd707fc8bfeb5fe4102db70180
Ricky Xie <xier892@newschool.edu>
2019-09-13 19:20:55 -0400

    use nested for loop to create pattern within pattern

--- src/js/main.js
+++ src/js/main.js
@@ -5,27 +5,25 @@ const grid = document.createElement('ul');
 grid.className = 'grid';
 
 const squares = [];
+const squares2 = [];
 
 for (let i = 0; i < 104; i++) {
   const lightness = i % 2 === 0 ? getRandomInt(20, 40) : getRandomInt(80, 100);
   const square = document.createElement('li');
-  square.style.width = '10%';
-  square.style.margin = '1%';
-  square.style.backgroundRepeat = 'repeat';
+  square.className = 'square';
   square.style.backgroundColor = `hsl(0, 0%, ${lightness}%)`;
-  switch (0) {
-    case i % 2:
-      square.style.backgroundImage = 'url("dist/assets/image/bedge-grunge.png")';
-      break;
-    case i % 3:
-      square.style.backgroundImage = 'url("dist/assets/image/bright-squares.png")';
-      break;
-    case i % 5:
-      square.style.backgroundImage = 'url("dist/assets/image/candyhole.png")';
-      break;
-    default:
-      square.style.backgroundImage = 'url("dist/assets/image/always-grey.png")';
+  const fragment2 = document.createDocumentFragment();
+  for (let j = 0; j < i; j++) {
+    const lightness2 = i % 2 === 0 ? getRandomInt(80, 100) : (20, 40);
+    const square2 = document.createElement('div');
+    square2.style.height = '100%';
+    square2.style.borderLeft = `1px solid hsl(0, 0%, ${lightness2}%)`;
+    square2.style.borderRight = `1px solid hsl(0, 0%, ${lightness2}%)`;
+    square2.style.width = `${100 / j}%`;
+    fragment2.appendChild(square2);
+    squares2.push(square2);
   }
+  square.appendChild(fragment2);
   fragment.appendChild(square);
   squares.push(square);
 }
